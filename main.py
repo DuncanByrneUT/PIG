3d764e6aa2ed99da115eb1cdaf3f9c683a2df9ac
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-30 16:07:33 -0500

    added a while loop to get the number of players in the game, with a max score variable

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ def roll():
     roll = random.randint(min_val, max_val)
 
     return roll
+while True:
+    players = input("Enter the number of players ( 1 - 4 ): ")
+    if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            break
+        else:
+            print("Must be between 1 - 4 players.")
+    else:
+        print("Invalid, try again.")
+
+max_score = 100
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+  pass
+
