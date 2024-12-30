35ebbee91cfbbe7066e4d1142212359a3eb12f4e
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-30 17:47:14 -0500

    added a max score tracker and it stops once the user reach the desired score whic h is 50, the game is finished

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ player_scores = [0 for _ in range(players)]
 # scoring system for the players
 while max(player_scores) < max_score:
     for player_index in range(players):
-        print("Player", player_index + 1, "turn has just started!")
+        print("\nPlayer", player_index + 1, "it sis now your turn!\n")
+        print("Your total score is: ", player_scores[player_index], "\n")
         current_score = 0
 
         while True:
@@ -43,3 +44,7 @@ while max(player_scores) < max_score:
 
         player_scores[player_index] += current_score
         print("Your total score is:", player_scores[player_index])
+
+max_score = max(player_scores)
+winning_index = player_scores.index(max_score)
+print("Player number", winning_index + 1, "is the winner with a score of:", max_score)
