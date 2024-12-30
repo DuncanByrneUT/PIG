e27356945e2617d5a209c2b72a8f552213a6b05b
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-30 17:38:23 -0500

    fixed the issues where the terminal would list all players turns starting on the same roll, game is close to finish might be last commit

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def roll():
     return roll
 # entering the amount of players and how many players are alowed to play the game
 while True:
-    players = input("Enter the number of players ( 1 - 4 ): ")
+    players = input("Enter the number of players ( 2 - 4 ): ")
     if players.isdigit():
         players = int(players)
-        if 1 <= players <= 4:
+        if 2 <= players <= 4:
             break
         else:
-            print("Must be between 1 - 4 players. ")
+            print("Must be between 2 - 4 players. ")
     else:
         print("Invalid, try again.")
 
@@ -23,23 +23,23 @@ player_scores = [0 for _ in range(players)]
 
 # scoring system for the players
 while max(player_scores) < max_score:
+    for player_index in range(players):
+        print("Player", player_index + 1, "turn has just started!")
+        current_score = 0
 
-  for player_index in range(players):
-    print("\nPlayer", player_index + 1, "turn has just started!\n")
-    current_score = 0
+        while True:
+            should_roll = input("Would you like to roll? (y)? ")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn over!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
+            print("Your score is: ", current_score)
 
-  while True:
-    should_roll = input("Would you like to roll (y)? ")
-    if should_roll.lower() != "y":
-      break
-    value = roll()
-    if value == 1:
-      print("You rolled a 1 turn over!")
-      current_score = 0
-    else:
-      current_score += value
-      print("You rolled a:", value)
-    print("Your score is:", current_score)
-
-player_scores[player_index] += current_score
-print("Your total score is:", player_scores[player_index])
+        player_scores[player_index] += current_score
+        print("Your total score is:", player_scores[player_index])
